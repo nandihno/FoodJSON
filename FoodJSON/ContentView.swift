feeeebef7424dd68a902fa86694faff7c9163cf5
Nando <fernando.deleon@gmail.com>
2022-08-06 22:39:42 +1000

    Initial Commit

---
+++ FoodJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodJSON
+//
+//  Created by Fernando De Leon on 6/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodJSON/FoodJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodJSONApp.swift
+//  FoodJSON
+//
+//  Created by Fernando De Leon on 6/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
